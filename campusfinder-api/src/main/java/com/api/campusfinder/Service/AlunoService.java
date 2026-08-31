98defa68c4015e084f71fedcd512ac3ac7366d72
euguissouza <contato.guisilva4@hotmail.com>
2026-08-31 15:10:22 -0300

    feat - new Service -> aluno

---
+++ campusfinder-api/src/main/java/com/api/campusfinder/Service/AlunoService.java
@@ -0,0 +1,26 @@
+package com.api.campusfinder.Service;
+
+import org.springframework.stereotype.Service;
+
+import com.api.campusfinder.DTO.AlunoDTO;
+import com.api.campusfinder.Entity.AlunoEntity;
+import com.api.campusfinder.Repository.AlunoRepository;
+
+@Service
+public class AlunoService {
+
+    private AlunoRepository repository;
+
+    public AlunoService(AlunoRepository repository) {
+        this.repository = repository;
+    }
+
+    public void cadastraAluno(AlunoDTO dto){
+        AlunoEntity aluno = new AlunoEntity();
+        aluno.setAluno(dto.getAluno());
+        aluno.setCampus(dto.getCampus());
+        aluno.setRgm(dto.getRgm());
+        repository.save(aluno);
+    }
+
+}
