164bf4595fef90b151ba755f0db6e4920a29f864
euguissouza <contato.guisilva4@hotmail.com>
2026-08-31 00:57:10 -0300

    feat - new Entity -> Aluno

---
+++ campusfinder-api/src/main/java/com/api/campusfinder/Entity/AlunoEntity.java
@@ -0,0 +1,54 @@
+package com.api.campusfinder.Entity;
+
+import com.api.campusfinder.Enum.Campus;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "aluno")
+public class AlunoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String aluno;
+    private String rgm;
+    private Campus campus;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getAluno() {
+        return aluno;
+    }
+    public void setAluno(String aluno) {
+        this.aluno = aluno;
+    }
+    public String getRgm() {
+        return rgm;
+    }
+    public void setRgm(String rgm) {
+        this.rgm = rgm;
+    }
+    public Campus getCampus() {
+        return campus;
+    }
+    public void setCampus(Campus campus) {
+        this.campus = campus;
+    }
+
+
+    @Override
+    public String toString() {
+        return "AlunoEntity [id=" + id + ", aluno=" + aluno + ", rgm=" + rgm + ", campus=" + campus + "]";
+    }
+
+    
+}
